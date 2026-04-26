798fd7bba360164bb6415e2694fee0700a0ec28d
Dinesh Jadhav <67954716+Dinesh9418@users.noreply.github.com>
2026-04-26 06:30:06 +0000

    light logo updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
 import Logo from "../../assets/logo.jpeg";
-import LightLogo from "../../assets/light-logo.svg";
+import LightLogo from "../../assets/light-logo.jpeg";
 
 import {
   RiHome2Line,
