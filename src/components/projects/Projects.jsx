8415d2c23e0b38a8add7625d65fedd38953ce5e8
Dinesh Jadhav <67954716+Dinesh9418@users.noreply.github.com>
2026-07-18 17:43:23 +0000

    technolgies added

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = [
         id: 2,
         project_name: "Profile Sketch",
         role: "web Developer",
-        echnologies: "HTML, CSS, JavaScript, ReactJS",
+        technologies: "HTML, CSS, JavaScript, ReactJS",
         yearsActive: "Dec 2021 - Jun 2022",
         description: [
             "Profile Sketch is a career-oriented platform designed to help students and professionals build, showcase, and manage their digital resumes and career profiles. The platform allows users to create interactive profiles, upload achievements, and connect with opportunities in a structured and visually appealing way."
@@ -24,17 +24,17 @@ const Projects = [
         responsibilities: ["Attend the sprint planning meeting", " Interpret requirements and documentation", " Develop assigned user stories", " Performing Dev testing", " Provide UAT support", " Provide post-production support"]
     },
 
-    // {
-    //     id: 1,
-    //     project_name: "MCA (Ministry of Corporate Affairs)",
-    //     role: "React Developer",
-    //     echnologies: "HTML, CSS, JavaScript, ReactJS, Bootstrap",
-    //     yearsActive: "Jul 2022 - Present",
-    //     description: [
-    //         "The MCA Corporate Compliance Portal is a web-based application designed to simplify compliance filing and tracking for corporate users. It enables companies to submit statutory forms online, validate entries, and monitor submission status, reducing manual paperwork and improving efficiency."
-    //     ],
-    //     responsibilities: ["●	Attend the sprint planning meeting", "●	Interpret requirements and documentation", "●	Develop assigned user stories", "●	Performing Dev testing", "●	Provide post-production support"]
-    // },
+    {
+        id: 3,
+        project_name: "Purexo Group",
+        role: "React Developer",
+        technologies: "HTML, CSS, JavaScript, ReactJS, Bootstrap,tailwind css, Firebase storage",
+        yearsActive: "May 2026 - Present",
+        description: [
+            "Developed a web-based Meal Tracker and Mess Management System for a hostel mess to simplify student meal and subscription management."
+        ],
+        responsibilities: ["	Attend the sprint planning meeting", "Interpret requirements and documentation", "Develop assigned user stories", "Performing Dev testing", " Provide post-production support"]
+    },
 
 ];
 export default Projects
